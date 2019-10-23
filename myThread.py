5fc165766abbf086a190c05f964da0f4de1cf235
Tedu <291311109@qq.com>
2019-10-23 10:36:29 +0800

    init commit

---
+++ myThread.py
@@ -0,0 +1,18 @@
+from threading import Thread
+from time import sleep,ctime
+
+class MyThread(Thread):
+    def __init__(self):
+        super().__init__()
+
+####################################################
+def player(sec,song):
+    for i in range(3):
+        print("Playing %s: %s"%(song,ctime()))
+        sleep(sec)
+
+t = MyThread(target = player,args=(3,),
+             kwargs={'song':'凉凉'})
+t.start()
+t.join()
+
